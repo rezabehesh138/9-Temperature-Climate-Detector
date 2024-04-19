8363555352960e0c19b6c03765905fb03b10cd23
rezabehesh138 <161194595+rezabehesh138@users.noreply.github.com>
2024-04-19 11:24:41 +0330

    Update Temperature Climate Detector Code.ino

--- Temperature Climate Detector Code.ino
+++ Temperature Climate Detector Code.ino
@@ -5,16 +5,16 @@ const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
-  pinMode(red, OUTPUT);
-  pinMode(green, OUTPUT);
+  pinMode(red, OUTPUT); //set pin 8 (LED) as output 
+  pinMode(green, OUTPUT);//set pin 9 (LED) as output
   Serial.begin(9600); //setup serial
-  lcd.begin(16, 2);
+  lcd.begin(16, 2); //lcd sise
 }
 
 void loop() {
   float voltage = analogRead(A0);  //read the inpute voltage (analog pin A0)
   float temp = voltage * (0.48828125); //computing Temp
-  lcd.setCursor(0, 0);
+  lcd.setCursor(0, 0);//Character Positioning
   lcd.print("Temp = "); //show Temp
   lcd.print(temp); //show Temp
   //  lcd.print("\xC2\xB0"); //show Temp
@@ -22,17 +22,17 @@ void loop() {
   // delay(2000); //waits for 2 second
 
   if (temp > 32) {
-    digitalWrite(red , HIGH);
-    digitalWrite(green , LOW);
-    lcd.setCursor(0, 1);
+    digitalWrite(red , HIGH);// red LED on
+    digitalWrite(green , LOW); // green LED off
+    lcd.setCursor(0, 1);//Character Positioning
     lcd.print("WARM"); //show Temp
     
 
   }
 
   else {
-    digitalWrite(green , HIGH);
-    digitalWrite(red , LOW);
+    digitalWrite(green , HIGH); // green LED on
+    digitalWrite(red , LOW);// red LED off
     lcd.setCursor(0, 1);
     lcd.print("COLD"); //show Temp
   }
